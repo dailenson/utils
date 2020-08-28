9f7d0097fbe8193c7c83f96b69172331e8203222
Gang Dai <dailleson@gmail.com>
2020-08-28 10:55:19 +0800

    Update process_data.py

--- process_data.py
+++ process_data.py
@@ -3,9 +3,10 @@ import shutil
 
 path = "/home/datasets/PACS"
 def process_data(source_dir, dist_dir):
-    for file_name in os.listdir(source_dir):
-        shutil.copy(os.path.join(source_dir, file_name), os.path.join(dist_dir, file_name))
-        if "train" in file_name:
+    for file_name in os.listdir(source_dir):     
+        if "val" in file_name:
+            pass       
+        else:
             file_path = os.path.join(source_dir, file_name)
             with open(file_path,'r') as file_to_read:
                 while True:
@@ -13,11 +14,19 @@ def process_data(source_dir, dist_dir):
                     if not lines:
                         break
                     _, label = [i for i in lines.split()] #数据的存放形式是数据名称+对应label，中间用空格隔开
-                    if int(label) < 4:
-                        temp_name = "meta_test_" + file_name
-                        with open(os.path.join(dist_dir, temp_name), "a") as f:
-                            f.write(lines)
-            print(file_name)
+                    if "train" in file_name:
+                        if int(label) < 4:
+                            temp_name = "meta_test_" + file_name
+                            with open(os.path.join(dist_dir, temp_name), "a") as f:
+                                f.write(lines)
+                            shutil.copy(os.path.join(source_dir, file_name), os.path.join(dist_dir, file_name))
+                        else:
+                            pass                    
+                    else:
+                        if int(label) < 4:
+                            with open(os.path.join(dist_dir, file_name), "a") as h:
+                                h.write(lines)
+            print(file_name)          
 
 if __name__ == "__main__":
     source_dir = os.path.join(path, "pacs_label_backup")
